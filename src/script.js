ff2a6be15996de16c52b82994268dfb348bb4ec3
CHR-onicles <tpandivine48@gmail.com>
2021-08-06 23:25:29 +0000

    adjustments

--- src/script.js
+++ src/script.js
@@ -10,10 +10,12 @@ nav_toggle.addEventListener("click", () => {
     if (container_height === 0){
         nav_links_container.style.height = `${nav_links_height}px`;
         nav_links_container.style.opacity = 1;
+        nav_links_container.style.visibility = 'visible';
         nav_links_container.style.transform = `translateY(0)`;
     } else {
         nav_links_container.style.height = 0;
         nav_links_container.style.opacity = 0;
+        nav_links_container.style.visibility = 'hidden';
         nav_links_container.style.transform = `translateY(-50px)`;
     }
 });
